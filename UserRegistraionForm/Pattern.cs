1c1fd5f47e66f3e9471a5852d54f53f008b74e3d
Chetan <Chetan@DESKTOP-9SU09BS>
2020-10-30 14:19:34 +0530

    [Chetan]Add. Validate users first name usinng Regex.

---
+++ UserRegistraionForm/Pattern.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistraionForm
+{
+    class Pattern
+    {
+        //Regex Pattern to validate First Name.
+        public string FIRST_NAME = "^[A-Z][a-zA-Z]{3,}";
+        /// <summary>
+        /// Validates the first name with regex pattern
+        /// First letter Capital and rest small.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <returns>True or false.</returns>
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, FIRST_NAME);
+        }
+    }
+}
