324b3cbc57cf08904e0d53fe5d9bb1487ec60100
Chetan <Chetan@DESKTOP-9SU09BS>
2020-10-30 17:11:30 +0530

    Revert "Revert "[Chetan]Add. Valid all Email Samples.""
    This reverts commit 08ea34d14a04287a5ec3517a9a530c7749e01353.

--- UserRegistraionForm/Program.cs
+++ UserRegistraionForm/Program.cs
@@ -21,6 +21,19 @@ namespace UserRegistraionForm
         {
             Console.WriteLine("Hello Welcome to User registration form...!");
             Pattern pattern = new Pattern();
+            
+            ///Reads User Name from user.
+            Console.WriteLine("Enter Last Name");
+            string userName = Console.ReadLine();
+            Console.WriteLine(pattern.ValidateUserName(userName));
+            ///Reads Email address from user.
+            Console.WriteLine("Enter Email Address");
+            string emailAddress = Console.ReadLine();
+            Console.WriteLine(pattern.ValidateEmailAddress(emailAddress));
+            ///Reads Mobile number from user.
+            Console.WriteLine("Enter your mobile number.");
+            string mobileNumber = Console.ReadLine();
+            Console.WriteLine(pattern.ValidateMobileNumber(mobileNumber));
             ///Reads passsword from user.
             Console.WriteLine("Enter Password");
             string password = Console.ReadLine();            
