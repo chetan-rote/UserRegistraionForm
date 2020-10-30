26217dceafdcad1f62e7db1f6e9ab4bc1d0ec227
Chetan <Chetan@DESKTOP-9SU09BS>
2020-10-30 14:07:25 +0530

    Add project files.

---
+++ UserRegistraionForm/Program.cs
@@ -0,0 +1,21 @@
+﻿/******************************************************************************
+ *  Purpose: User Registration System Ensures all validations are in place
+ *           during User Entry.   
+ *
+ *  @author  Chetan
+ *  @version 1.0
+ *  @since   26-10-2020
+ *  
+ ******************************************************************************/
+using System;
+
+namespace UserRegistraionForm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome to User registration form...!");
+        }
+    }
+}
